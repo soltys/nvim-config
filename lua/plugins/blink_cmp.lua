a321a350c9d43dcbf914ec0d79d38ef4106f4cca
Paweł Sołtysiak <pawel@soltysiak.it>
2025-02-02 18:36:20 +0100

    snacks.picker keymap settings

--- lua/plugins/blink_cmp.lua
+++ lua/plugins/blink_cmp.lua
@@ -1,17 +1,27 @@
 return {
     {
         "saghen/blink.cmp",
+        --- @type blink.cmp.ConfigStrict
         opts = {
             enabled = function()
                 return not vim.tbl_contains({ "markdown", "DressingInput" }, vim.bo.filetype)
                     and vim.bo.buftype ~= "prompt"
                     and vim.b.completion ~= false
             end,
-            sources = {
-                default = { "lsp", "path", "snippets" },
-            },
             keymap = {
                 preset = "super-tab",
+            },
+            sources = {
+                -- add lazydev to your completion providers
+                default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+                providers = {
+                    lazydev = {
+                        name = "LazyDev",
+                        module = "lazydev.integrations.blink",
+                        -- make lazydev completions top priority (see `:h blink.cmp`)
+                        score_offset = 100,
+                    },
+                },
             },
             completion = {
                 list = {
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,6 +6,18 @@ return {
         },
         ---@type snacks.picker.Config
         picker = {
+            win = {
+                input = {
+                    keys = {
+                        ["C-e"] = { "confirm", mode = { "n", "i" } },
+                    },
+                },
+                list = {
+                    keys = {
+                        ["C-e"] = { "confirm" },
+                    },
+                },
+            },
             ---@type  [string] snacks.picker.sources.Config | {}
             sources = {
                 ---@type snacks.picker.grep.Config
@@ -15,6 +27,7 @@ return {
                     live = true, -- live grep by default
                     ft = "xaml",
                     supports_live = true,
+                    show_empty = true,
                 },
             },
         },
