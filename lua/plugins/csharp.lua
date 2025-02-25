a544738532276d223a77f4bdd5767f673c700e1c
Paweł Sołtysiak <pawel@soltysiak.it>
2025-02-25 09:15:56 +0100

    removed some extras which was not used

--- lua/plugins/csharp.lua
+++ lua/plugins/csharp.lua
@@ -9,12 +9,6 @@ return {
     {
         "seblyng/roslyn.nvim",
         ft = "cs",
-        opts = {
-            filewatching = true,
-            ignore_target = function(target)
-                return string.match(target, "Phoenix.sln") ~= nil
-            end,
-            -- your configuration comes here; leave empty for default settings
-        },
+        opts = {},
     },
 }
