4417a76b6a9c8e40cff050be798e38a21b6c79d6
Paweł Sołtysiak <pwso@demant.com>
2025-05-22 14:08:18 +0200

    added blink setting and nvim config

--- lua/svim/lsp.lua
+++ lua/svim/lsp.lua
@@ -1,5 +1,53 @@
-vim.lsp.enable("lua_ls")
-vim.lsp.enable("powershell_es")
+vim.lsp.config("*", {
+    -- options for vim.diagnostic.config()
+    ---@type vim.diagnostic.Opts
+    diagnostics = {
+        underline = true,
+        update_in_insert = false,
+        virtual_text = {
+            spacing = 4,
+            source = "if_many",
+            prefix = "●",
+            -- this will set set the prefix to a function that returns the diagnostics icon based on the severity
+            -- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
+            -- prefix = "icons",
+        },
+        severity_sort = true,
+        signs = {
+            text = {
+                [vim.diagnostic.severity.ERROR] = "",
+                [vim.diagnostic.severity.WARN] = "",
+                [vim.diagnostic.severity.HINT] = "",
+                [vim.diagnostic.severity.INFO] = "",
+            },
+        },
+    },
+    -- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
+    -- Be aware that you also will need to properly configure your LSP server to
+    -- provide the inlay hints.
+    inlay_hints = {
+        enabled = true,
+    },
+    -- Enable this to enable the builtin LSP code lenses on Neovim >= 0.10.0
+    -- Be aware that you also will need to properly configure your LSP server to
+    -- provide the code lenses.
+    codelens = {
+        enabled = true,
+    },
+    -- add any global capabilities here
+    capabilities = {
+        workspace = {
+            fileOperations = {
+                didRename = true,
+                willRename = true,
+            },
+        },
+    },
+    format = {
+        formatting_options = nil,
+        timeout_ms = nil,
+    },
+})
 vim.lsp.config("powershell_es", {
     bundle_path = vim.fn.stdpath("data") .. "/mason/packages/powershell-editor-services",
     filetypes = { "ps1", "psm1", "psd1" },
