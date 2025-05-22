52f6d7ac1ee51a417b3e68b4348a4616a0e77dff
Paweł Sołtysiak <pwso@demant.com>
2025-05-22 10:17:49 +0200

    added TreeWalker as plugin

--- lua/svim/keymaps.lua
+++ lua/svim/keymaps.lua
@@ -11,10 +11,10 @@ map("n", "<Esc>", "<cmd>nohlsearch<CR>")
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-map("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-map("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-map("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-map("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+map("n", "<M-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+map("n", "<M-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+map("n", "<M-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+map("n", "<M-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- buffers
 map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
--- lua/svim/plugins/coding.lua
+++ lua/svim/plugins/coding.lua
@@ -1,5 +1,40 @@
 return {
     {
+        "aaronik/treewalker.nvim",
+        opts = {
+            -- Whether to briefly highlight the node after jumping to it
+            highlight = true,
+
+            -- How long should above highlight last (in ms)
+            highlight_duration = 250,
+
+            -- The color of the above highlight. Must be a valid vim highlight group.
+            -- (see :h highlight-group for options)
+            highlight_group = "CursorLine",
+
+            -- Whether the plugin adds movements to the jumplist -- true | false | 'left'
+            --  true: All movements more than 1 line are added to the jumplist. This is the default,
+            --        and is meant to cover most use cases. It's modeled on how { and } natively add
+            --        to the jumplist.
+            --  false: Treewalker does not add to the jumplist at all
+            --  "left": Treewalker only adds :Treewalker Left to the jumplist. This is usually the most
+            --          likely one to be confusing, so it has its own mode.
+            jumplist = true,
+        },
+        keys = {
+            -- swapping
+            { "<C-S-k>", "<cmd>Treewalker SwapUp<cr>", mode = { "n" }, silent = true },
+            { "<C-S-j>", "<cmd>Treewalker SwapDown<cr>", mode = { "n" }, silent = true },
+            { "<C-S-h>", "<cmd>Treewalker SwapLeft<cr>", mode = { "n" }, silent = true },
+            { "<C-S-l>", "<cmd>Treewalker SwapRight<cr>", mode = { "n" }, silent = true },
+            -- movement
+            { "<C-k>", "<cmd>Treewalker Up<cr>", mode = { "n", "v" }, silent = true },
+            { "<C-j>", "<cmd>Treewalker Down<cr>", mode = { "n", "v" }, silent = true },
+            { "<C-h>", "<cmd>Treewalker Left<cr>", mode = { "n", "v" }, silent = true },
+            { "<C-l>", "<cmd>Treewalker Right<cr>", mode = { "n", "v" }, silent = true },
+        },
+    },
+    {
         "seblyng/roslyn.nvim",
         ft = "cs",
         ---@module 'roslyn.config'
@@ -44,7 +79,6 @@ return {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         main = "nvim-treesitter.configs", -- Sets main module to use for opts
-        -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
         opts = {
             ensure_installed = {
                 "bash",
