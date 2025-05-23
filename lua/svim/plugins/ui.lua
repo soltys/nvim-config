fc00c7306ad06f5ec481dc92ff4ce69f2162ef87
Paweł Sołtysiak <pwso@demant.com>
2025-05-23 13:26:04 +0200

    added config for Lualine

--- lua/svim/plugins/ui.lua
+++ lua/svim/plugins/ui.lua
@@ -5,18 +5,33 @@ return {
         dependencies = "nvim-tree/nvim-web-devicons",
         ---@type bufferline.Options
         opts = {
-			options={
-				close_command = function(n)
-					Snacks.bufdelete(n)
-				end,
-				right_mouse_command = function(n)
-					Snacks.bufdelete(n)
-				end,
-			}
+            options = {
+                close_command = function(n)
+                    Snacks.bufdelete(n)
+                end,
+                right_mouse_command = function(n)
+                    Snacks.bufdelete(n)
+                end,
+            },
         },
     },
     {
         "nvim-lualine/lualine.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
+        event = "VeryLazy",
+        opts = {
+            options = {
+                icons_enabled = true,
+                theme = "auto",
+            },
+            sections = {
+                lualine_a = { "mode" },
+                lualine_b = { "filename" },
+                lualine_c = {},
+                lualine_x = { "encoding", "fileformat", "filetype" },
+                lualine_y = { "progress" },
+                lualine_z = { "location" },
+            },
+        },
     },
 }
