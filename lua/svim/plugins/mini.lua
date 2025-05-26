3fea6d4037731252308c765e0cd98ade4b5310d8
Paweł Sołtysiak <pwso@demant.com>
2025-05-26 10:58:29 +0200

    keep working on svim config

--- lua/svim/plugins/mini.lua
+++ lua/svim/plugins/mini.lua
@@ -2,7 +2,6 @@
 
 local utils = require("svim.utils")
 local lib = require("svim.lib")
-
 return {
     "echasnovski/mini.nvim",
     event = "VeryLazy",
@@ -22,15 +21,15 @@ return {
                     i = { "@block.inner", "@conditional.inner", "@loop.inner" },
                 }),
                 f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
-                c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
-                t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
-                d = { "%f[%d]%d+" }, -- digits
-                e = { -- Word with case
+                c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }),       -- class
+                t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },           -- tags
+                d = { "%f[%d]%d+" },                                                          -- digits
+                e = {                                                                         -- Word with case
                     { "%u[%l%d]+%f[^%l%d]", "%f[%S][%l%d]+%f[^%l%d]", "%f[%P][%l%d]+%f[^%l%d]", "^[%l%d]+%f[^%l%d]" },
                     "^().*()$",
                 },
-                g = utils.mini.ai.buffer, -- buffer
-                u = ai.gen_spec.function_call(), -- u for "Usage"
+                g = utils.mini.ai.buffer,                                  -- buffer
+                u = ai.gen_spec.function_call(),                           -- u for "Usage"
                 U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }), -- without dot in function name
             },
         }
@@ -44,14 +43,27 @@ return {
 
         require("mini.surround").setup({
             mappings = {
-                add = "gsa", -- Add surrounding in Normal and Visual modes
-                delete = "gsd", -- Delete surrounding
-                find = "gsf", -- Find surrounding (to the right)
-                find_left = "gsF", -- Find surrounding (to the left)
-                highlight = "gsh", -- Highlight surrounding
-                replace = "gsr", -- Replace surrounding
+                add = "gsa",            -- Add surrounding in Normal and Visual modes
+                delete = "gsd",         -- Delete surrounding
+                find = "gsf",           -- Find surrounding (to the right)
+                find_left = "gsF",      -- Find surrounding (to the left)
+                highlight = "gsh",      -- Highlight surrounding
+                replace = "gsr",        -- Replace surrounding
                 update_n_lines = "gsn", -- Update `n_lines`
             },
+        })
+
+        require("mini.pairs").setup({
+            modes = { insert = true, command = true, terminal = false },
+            -- skip autopair when next character is one of these
+            skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+            -- skip autopair when the cursor is inside these treesitter nodes
+            skip_ts = { "string" },
+            -- skip autopair when next character is closing pair
+            -- and there are more closing pairs than opening pairs
+            skip_unbalanced = true,
+            -- better deal with markdown code blocks
+            markdown = true,
         })
     end,
 }
