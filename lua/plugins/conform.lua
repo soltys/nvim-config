9436f468b4d90c6162beda22f19a8056e9eb36bd
Paweł Sołtysiak <pawel@soltysiak.it>
2025-06-03 11:25:44 +0200

    removed LazyVim override config

--- lua/plugins/conform.lua
+++
@@ -1,9 +0,0 @@
-return {
-    {
-        "stevearc/conform.nvim",
-        ---@type conform.setupOpts
-        opts = {
-            default_format_opts = { timeout_ms = 10000 },
-        },
-    },
-}
