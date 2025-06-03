a74c2016639704ad508eacdd8fac299a2a249b01
Paweł Sołtysiak <pawel@soltysiak.it>
2025-06-03 11:25:55 +0200

    [svim] 1.0

--- lua/svim/plugins/file_management.lua
+++ lua/svim/plugins/file_management.lua
@@ -5,8 +5,7 @@ return {
         ---@type oil.SetupOpts
         opts = {},
         -- Optional dependencies
-        dependencies = { { "echasnovski/mini.nvim", opts = {} } },
-        -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+        dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
         -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
         lazy = false,
     },
