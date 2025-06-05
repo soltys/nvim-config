2bca635fea56dc6b58b2b831adabcb9263966c13
Paweł Sołtysiak <pawel@soltysiak.it>
2025-06-05 07:46:02 +0200

    [svim] Updated Treesitter config, which installs parsers from config

--- lua/svim/autocmds.lua
+++ lua/svim/autocmds.lua
@@ -1,17 +1,32 @@
+local function augroup(name)
+    return vim.api.nvim_create_augroup("svim_" .. name, { clear = true })
+end
+
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.hl.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
     desc = "Highlight when yanking (copying) text",
-    group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+    group = augroup("highlight-yank"),
     callback = function()
         vim.hl.on_yank()
     end,
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function(args)
-    require("conform").format({ bufnr = args.buf })
-  end,
+    pattern = "*",
+    group = augroup("conform_format_on_write"),
+    callback = function(args)
+        require("conform").format({ bufnr = args.buf })
+    end,
+})
+
+-- wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd("FileType", {
+    group = augroup("wrap_spell"),
+    pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
+    callback = function()
+        vim.opt_local.wrap = true
+        vim.opt_local.spell = true
+    end,
 })
