588179607ca2d746d2a1b8f92e63aaffc4eff469
Paweł Sołtysiak <pawel@soltysiak.it>
2025-06-26 08:04:52 +0200

    [svim] mason updating the list of used lsp and tools

--- lua/svim/plugins/mason.lua
+++ lua/svim/plugins/mason.lua
@@ -9,29 +9,26 @@ return {
         },
     },
     {
-        "mason-org/mason-lspconfig.nvim",
-        opts = {
-            ensure_installed = {
-                "lua_ls",
-                "clangd",
-                "powershell_es",
-            },
-        },
         dependencies = {
             "mason-org/mason.nvim",
             "neovim/nvim-lspconfig",
         },
+        "mason-org/mason-lspconfig.nvim",
     },
     {
         "WhoIsSethDaniel/mason-tool-installer.nvim",
         event = "VeryLazy",
         dependencies = {
             "mason-org/mason.nvim",
+            "mason-org/mason-lspconfig.nvim",
         },
         opts = {
             ensure_installed = {
                 "stylua",
                 "roslyn",
+                "lua_ls",
+                "clangd",
+                "powershell_es",
             },
         },
     },
