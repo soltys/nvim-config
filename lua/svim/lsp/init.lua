d6234c7692b8b0c0600cd2f5ea4b1c3e7c58eae7
Paweł Sołtysiak <pawel@soltysiak.it>
2025-07-16 08:48:46 +0200

    [svim] merge files into smaller subset

--- lua/svim/lsp/init.lua
+++ lua/svim/lsp/init.lua
@@ -1,48 +1,30 @@
-local lsp_keymap = require("svim.lsp.keymaps")
+local M = {}
+function M.setup()
+    vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("my.lsp", {}),
+        callback = function(args)
+            local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+            require("svim.lsp.keymaps").on_attach(client, args.buf)
+            if
+                not client:supports_method("textDocument/willSaveWaitUntil")
+                and client:supports_method("textDocument/formatting")
+            then
+                vim.api.nvim_create_autocmd("BufWritePre", {
+                    group = vim.api.nvim_create_augroup("my.lsp", { clear = false }),
+                    buffer = args.buf,
+                    callback = function()
+                        vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+                    end,
+                })
+            end
+        end,
+    })
 
-vim.api.nvim_create_autocmd("LspAttach", {
-    group = vim.api.nvim_create_augroup("my.lsp", {}),
-    callback = function(args)
-        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-        lsp_keymap.on_attach(client, args.buf)
-        if
-            not client:supports_method("textDocument/willSaveWaitUntil")
-            and client:supports_method("textDocument/formatting")
-        then
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = vim.api.nvim_create_augroup("my.lsp", { clear = false }),
-                buffer = args.buf,
-                callback = function()
-                    vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
-                end,
-            })
-        end
-    end,
-})
-vim.diagnostic.config({
-    virtual_text = true,
-    update_in_insert = true,
-    underline = true,
-    severity_sort = true,
-    float = {
-        focusable = true,
-        style = "minimal",
-        border = "rounded",
-        source = true,
-        header = "",
-        prefix = "",
-    },
-    signs = {
-        text = {
-            [vim.diagnostic.severity.HINT] = " ",
-            [vim.diagnostic.severity.INFO] = " ",
-            [vim.diagnostic.severity.WARN] = " ",
-            [vim.diagnostic.severity.ERROR] = " ",
-        },
-    },
-})
+    vim.lsp.enable("roslyn")
+    vim.lsp.config("powershell_es", require("svim.lsp.powershell"))
+    vim.lsp.enable("gopls")
+    vim.lsp.enable("clangd")
+    vim.lsp.enable("lua_ls")
+end
 
-vim.lsp.enable("roslyn")
-vim.lsp.config("powershell_es", require("svim.lsp.powershell"))
-vim.lsp.enable("gopls")
-vim.lsp.enable("clangd")
+return M
