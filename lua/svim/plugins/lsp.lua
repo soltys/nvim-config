bf79b958fef56d379fcd91fe75f335b62832be50
Paweł Sołtysiak <pawel@soltysiak.it>
2025-09-30 12:25:45 +0200

    removed ray-x/go.nvim

--- lua/svim/plugins/lsp.lua
+++ lua/svim/plugins/lsp.lua
@@ -49,9 +49,7 @@ return {
         ---@type RoslynNvimConfig
         opts = {
             -- ignores all targets, forces to manually choose target, stops auto launching roslnyn
-            ignore_target = function(_)
-                return true
-            end,
+            ignore_target = function(_) return true end,
         },
     },
     {
@@ -65,27 +63,5 @@ return {
                 { path = "wezterm-types", modes = { "wezterm" } },
             },
         },
-    },
-    {
-        "ray-x/go.nvim",
-        dependencies = { -- optional packages
-            "ray-x/guihua.lua",
-            "neovim/nvim-lspconfig",
-            "nvim-treesitter/nvim-treesitter",
-        },
-        config = function(lp, opts)
-            require("go").setup(opts)
-            local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                pattern = "*.go",
-                callback = function()
-                    require("go.format").goimports()
-                end,
-                group = format_sync_grp,
-            })
-        end,
-        event = { "CmdlineEnter" },
-        ft = { "go", "gomod" },
-        -- build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
     },
 }
