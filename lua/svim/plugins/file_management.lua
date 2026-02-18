9b1a724b1abfe5921994c27d62de54aaafa08a3f
Paweł Sołtysiak <pawel@soltysiak.it>
2026-02-18 13:53:37 +0100

    changed oil shortcut

--- lua/svim/plugins/file_management.lua
+++ lua/svim/plugins/file_management.lua
@@ -1,30 +1,19 @@
-local lib = require("svim.lib")
 return {
     {
         "stevearc/oil.nvim",
         ---@module 'oil'
         ---@type oil.SetupOpts
+        keys = {
+            {
+                "<leader>o",
+                function() require("oil").open(vim.fn.getcwd()) end,
+                desc = "Open Oil",
+            },
+        },
         opts = {},
         -- Optional dependencies
         dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
         -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
         lazy = false,
-        config = function(opts)
-            require("oil").setup(opts)
-            lib.on_load("mini.surround", function()
-                vim.schedule(function()
-                    require("which-key").add({
-                        {
-                            "<leader>fo",
-                            function()
-                                require("oil").open(vim.fn.getcwd())
-                            end,
-                            desc = "Open Oil",
-                            icon = "󰏇",
-                        },
-                    })
-                end)
-            end)
-        end,
     },
 }
