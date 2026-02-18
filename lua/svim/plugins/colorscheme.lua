9fcb06e5e0338eab46854aec855125eebe483898
Paweł Sołtysiak <pawel@soltysiak.it>
2026-02-18 13:51:30 +0100

    [svim] modfied coloring of line numbers, addded dial plugin, modified readme

--- lua/svim/plugins/colorscheme.lua
+++ lua/svim/plugins/colorscheme.lua
@@ -3,7 +3,18 @@ return {
     name = "catppuccin",
     priority = 1000,
     config = function()
-        require("catppuccin").setup()
+        ---@type CatppuccinOptions
+        local opts = {
+            highlight_overrides = {
+                frappe = function(frappe)
+                    return {
+                        LineNr = { fg = frappe.green },
+                        CursorLineNr = { fg = frappe.red },
+                    }
+                end,
+            },
+        }
+        require("catppuccin").setup(opts)
         -- setup must be called before loading
         vim.cmd.colorscheme(vim.g.colorscheme)
     end,
